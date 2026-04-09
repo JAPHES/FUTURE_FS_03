fc1c0bc659c1a80c0ac539ced2002ff24e84c798
JAPHES <japhesmurithi@gmail.com>
2026-04-09 14:44:44 +0300

    Allow flexible usernames in Django superuser creation

--- core/apps.py
+++ core/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        username_field = User._meta.get_field("username")
+        username_field.validators = []
+        username_field.help_text = ""
